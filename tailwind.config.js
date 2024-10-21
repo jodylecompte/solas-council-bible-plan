ddeb4d249a9e8683e318200e9ef14a8589276aa6
Jody LeCompte <jody@jodylecompte.com>
2024-10-21 17:40:36 -0500

    add daisy-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -18,5 +19,5 @@ export default {
         },
     },
 
-    plugins: [forms, require('@tailwindcss/typography')],
+    plugins: [forms, require('@tailwindcss/typography'), require('daisyui')],
 };
