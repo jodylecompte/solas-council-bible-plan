c2c6fe1b233cb4740501003ce1a3e166c6c4cdb2
Jody LeCompte <jody@jodylecompte.com>
2024-10-25 16:41:49 -0500

    Fix migation issue blocking migrate:fresh + add progress fields

--- database/migrations/2024_10_30_205332_create_plans_table.php
+++ database/migrations/2024_10_30_205332_create_plans_table.php
@@ -15,11 +15,23 @@ return new class extends Migration {
             $table->integer('day');
             $table->string('nt_reading');
             $table->string('ot_reading');
+
+            $table->tinyInteger('progress_ot');
+            $table->tinyInteger('progress_nt');
+            $table->tinyInteger('progress_creed');
+            $table->tinyInteger('progress_catechism');
+            $table->tinyInteger('progress_confession');
+
+
+
+            $table->unsignedBigInteger('user_id')->nullable();
+
             $table->unsignedBigInteger('creed_id');
             $table->unsignedBigInteger('catechism_id');
             $table->unsignedBigInteger('confession_id');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on(table: 'users');
+
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('creed_id')->references('id')->on('creeds');
             $table->foreign('catechism_id')->references('id')->on('catechisms');
             $table->foreign('confession_id')->references('id')->on('confessions');
