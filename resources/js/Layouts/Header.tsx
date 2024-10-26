09e0eb430de0305ae899c53106173820af37775f
Jody LeCompte <jody@jodylecompte.com>
2024-10-26 15:28:43 -0500

    Fix error in logout function

--- resources/js/Layouts/Header.tsx
+++ resources/js/Layouts/Header.tsx
@@ -1,32 +1,21 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Link } from '@inertiajs/react';
-import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { Link, useForm } from '@inertiajs/react';
+
 import { useAuth } from '@/Context/AuthContext';
 
-import { FaUser } from 'react-icons/fa';
-
 export const Header = (props: any) => {
-  const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
   const { user } = useAuth();
+  const { post } = useForm();
 
-  const AuthNavItems = () => <div>Logged In</div>;
-  const GuestNavItems = () => <div>Guest</div>;
+  const logout = () => {
+    post(route('logout'), {
+      onSuccess: () => {
+        console.log('Logged out successfully!');
+      },
+    });
+  };
 
   return (
-    // <header className="bg-[#7C2424] text-white p-8 px-6 text-center md:text-left md:px-32 flex flex-col md:flex-row justify-between items-center">
-    //   <h1 className="text-2xl font-bold">
-    //     <Link href="/">The Solas Council Reformed Bible Study Plan</Link>
-    //   </h1>
-    //   <nav className="flex gap-7 pt-6 md:pt-0">
-    //     <Link href="/about">About</Link>
-    //     <Link href="/plan">Plan</Link>
-    //     {user && <Link href="/profile">Profile</Link>}
-    //     {!user && <Link href="/login">Login</Link>}
-    //   </nav>
-    // </header>
     <div className="bg-[#7C2424]">
       <div className="navbar text-white max-w-6xl mx-auto flex flex-col items-center justify-center md:flex-row">
         <div className="flex-1">
@@ -53,10 +42,16 @@ export const Header = (props: any) => {
                   {user && (
                     <>
                       <li>
-                        <Link href="/profile">Profile</Link>
+                        <Link className="btn btn-ghost" href="/profile">
+                          Profile
+                        </Link>
                       </li>
                       <li>
-                        <Link href="/logout">Logout</Link>
+                        <form className="btn btn-ghost" onSubmit={logout}>
+                          <button className="" type="submit">
+                            Logout
+                          </button>
+                        </form>
                       </li>
                     </>
                   )}
