18362d1b9a196d85342816e8fc0f740e1c4e2371
Jody LeCompte <jody@jodylecompte.com>
2024-10-26 15:51:37 -0500

    Remove all refereneces / redirects to dashboard component

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('Plan.Start', absolute: false));
+        return redirect()->intended(route('plan.start', absolute: false));
     }
 
     /**
