a5fea21682f7584ffd46d62b8b3de4d035f5b158
Jody LeCompte <jody@jodylecompte.com>
2024-11-02 13:19:15 -0500

    Fix missed import removal

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,7 +4,6 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot, hydrateRoot } from 'react-dom/client';
-import { AuthProvider } from './Context/AuthContext';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
